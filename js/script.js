11f75895686246ab03c0f2fa3aea22bd6b50ce44
turptori2 <turptori@umich.edu>
2025-12-15 16:03:24 -0500

    button changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
+document.querySelector('#click-me').addEventListener('click', () => {
+    document.querySelectorAll('div').forEach((div) => div.remove());
+})
 
